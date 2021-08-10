ee010233a0c8604c1db6a69cca8f8e78d2388837
PGabriel-MB <pablogrbarbosa@gmail.com>
2021-08-09 21:43:51 -0300

    primeiro commit

---
+++ src/test/java/br/gov/sp/fatec/CarManagement/CarManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.CarManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
