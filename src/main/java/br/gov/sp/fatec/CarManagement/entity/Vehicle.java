c46744cd4be2f2d4ad137a5544f02c16f5ada118
PabloGabriel <pablogrbarbosa@gmail.com>
2021-08-24 01:16:13 +0000

    implementada a entidade de veiculo

---
+++ src/main/java/br/gov/sp/fatec/CarManagement/entity/Vehicle.java
@@ -0,0 +1,67 @@
+package br.gov.sp.fatec.CarManagement.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vehicle")
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "vehicle")
+    private String vehicle;
+
+    @Column(name = "brand")
+    private String brand;
+    
+    @Column(name = "year")
+    private Integer year;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "sold")
+    private Boolean sold;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
