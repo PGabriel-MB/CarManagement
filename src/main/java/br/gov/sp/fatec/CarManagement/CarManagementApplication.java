42c7492aa0f9a117d6857bcbffa12a12d6cf51e7
PabloGabriel <pablogrbarbosa@gmail.com>
2021-09-07 00:49:49 +0000

    iniciando conexao com banco de dados

--- src/main/java/br/gov/sp/fatec/CarManagement/CarManagementApplication.java
+++ src/main/java/br/gov/sp/fatec/CarManagement/CarManagementApplication.java
@@ -2,9 +2,17 @@ package br.gov.sp.fatec.CarManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.*;
 
 @SpringBootApplication
 public class CarManagementApplication {
+	private static void connect() {
+		try (Connection connection = DriverManager.getConnection("jdbc:sqlite:database.db")) {
+			System.out.println("Connection established successfully!");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarManagementApplication.class, args);
